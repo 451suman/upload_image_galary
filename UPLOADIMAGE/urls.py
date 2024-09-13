65118674574fa310916be0fcd03a2aea93cd8794
451suman <sumanmushyakhwo@gmail.com>
2024-09-13 17:14:59 +0545

    logout completed

--- UPLOADIMAGE/urls.py
+++ UPLOADIMAGE/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('uploadimage_app.urls')),  # Ensure the app's URLs are correctly included
+    path('', include('uploadimage_app.urls')), 
+      path("accounts/login/", views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", views.LogoutView.as_view(), name="logout"), 
 ]
 
 if settings.DEBUG:
